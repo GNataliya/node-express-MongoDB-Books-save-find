f1c5280a0ee313e2fac4f5daae4f65506283a22a
GNataliya <80199130+GNataliya@users.noreply.github.com>
2021-09-06 21:12:47 +0300

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -13,44 +13,28 @@ router.get('/', (req, res) => {
 
 router.post('/server',  async (req, res) => { 
   const getData = await sendArticles.findArticles();
-  //console.log(getData)
   res.json(getData);
 });
 
 router.post('/form', upload.none(), async (req, res) => {
-  //console.log(req.body)
-  
-  const articleNew  =  sendArticles.saveArticle(req.body);     //сохранение в базу работает!!!!!!!!!!!!
-
-  // const selectArticles = await sendArticles.selectName( 'name -_id' );
-  //console.log('names', selectArticles)
-
-  //res.json(selectArticles);
+  const articleNew  =  sendArticles.saveArticle(req.body);    
   res.json(articleNew)
-  //res.json({ status: 'ok' });
 });
 
 
 router.post('/find', upload.none(), async (req, res) => {
-  //console.log(req.body)
-  
   const { articleTitle } = req.body;          // выводим имя для поиска из обьекта
   const titles = await sendArticles.findInfoInArticle(articleTitle);
-  //console.log(titles)                       // поиск документа (строки) по имени, работает !!!!!
-
   res.json(titles);
 });
 
 
 router.post('/change', upload.none(), async (req, res) => {
-  //console.log(req.body)
-  
   const { articleTitle } = req.body;
   const { text } = req.body;
   const updateText = await sendArticles.updateText(articleTitle, text);
   
   res.json(updateText);
-  //res.json({ status: 'ok' })
 });
 
 
